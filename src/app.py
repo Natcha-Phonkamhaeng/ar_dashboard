b8e4f290c375c16ecdb4e8830d15591453bdadb5
natcha phonkamhaeng <natcha.phonkamhaeng@gmail.com>
2023-08-30 00:33:09 +0700

    app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 from dash import Dash, html, Output, Input, dcc
 
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SPACELAB, dbc.icons.BOOTSTRAP])
-server = app.server
+
 
 magenta = '#db0f72'
 
@@ -13,7 +13,7 @@ SIDEBAR_STYLE = {
     "top": 0,
     "left": 0,
     "bottom": 0,
-    "width": "18rem",
+    "width": "13rem",
     "padding": "2rem 1rem",
     "background-color": magenta #"#f8f9fa",
 }
@@ -21,7 +21,7 @@ SIDEBAR_STYLE = {
 # the styles for the main content position it to the right of the sidebar and
 # add some padding.
 CONTENT_STYLE = {
-    "margin-left": "5rem",
+    "margin-left": "10rem",
     #"margin-right": '5rem',
     #'padding': '2rem'
     #'margin-right': '10%',
@@ -30,19 +30,16 @@ CONTENT_STYLE = {
 
 sidebar = html.Div([
     dbc.Card([
-        dbc.CardBody([
-            html.Img(src='assets/logo.png'),
-            ],className='text-center',
-            )
-        ]),
+        dbc.CardImg(src='assets/logo.png', style={'margin-top': '5px'})
+        ], style={'width': '8rem', 'height': '5rem', 'margin-left': '18px'}),
     html.Br(),
-    html.Div(['Created By', html.Br()], style={'height': '1px', 'font-size':'18px', 'color':'white'}),
+    html.Div(['Created By', html.Br()], style={'height': '1px', 'font-size':'13px', 'color':'white'}),
     html.Br(),
     html.A(children=[html.Div(['Natcha Phonkamhaeng'])], 
         href='https://natcha-p-resume.onrender.com/', 
         title='About Me',
         target='_blank',
-        style={'height': '1px', 'font-size':'18px', 'color':'white'}),
+        style={'height': '1px', 'font-size':'15px', 'color':'white'}),
     html.Hr(
         style={
             "borderWidth": "0.3vh",
