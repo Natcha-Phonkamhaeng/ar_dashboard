b1b87c281631074a90e5e44406d2e30c91dd1841
natcha phonkamhaeng <natcha.phonkamhaeng@gmail.com>
2023-08-29 23:12:27 +0700

    test

---
+++ src/app.py
@@ -0,0 +1,81 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import Dash, html, Output, Input, dcc
+
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SPACELAB, dbc.icons.BOOTSTRAP])
+server = app.server
+
+magenta = '#db0f72'
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "18rem",
+    "padding": "2rem 1rem",
+    "background-color": magenta #"#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "5rem",
+    #"margin-right": '5rem',
+    #'padding': '2rem'
+    #'margin-right': '10%',
+    "margin-top": "2em"
+}
+
+sidebar = html.Div([
+    dbc.Card([
+        dbc.CardBody([
+            html.Img(src='assets/logo.png'),
+            ],className='text-center',
+            )
+        ]),
+    html.Br(),
+    html.Div(['Created By', html.Br()], style={'height': '1px', 'font-size':'18px', 'color':'white'}),
+    html.Br(),
+    html.A(children=[html.Div(['Natcha Phonkamhaeng'])], 
+        href='https://natcha-p-resume.onrender.com/', 
+        title='About Me',
+        target='_blank',
+        style={'height': '1px', 'font-size':'18px', 'color':'white'}),
+    html.Hr(
+        style={
+            "borderWidth": "0.3vh",
+            "width": "100%",
+            "borderColor": "#3294a8",
+            "opacity": "unset",
+        }
+        ),
+    dbc.Nav([
+        dbc.NavLink([
+            html.Div(page['name'], className='ms-2 text-white')
+            ],
+            href=page['path'],
+            active='exact',
+            )
+            for page in dash.page_registry.values()
+        ], 
+        vertical=True, pills=True),
+    ])
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            sidebar
+            ],
+            style=SIDEBAR_STYLE),
+        dbc.Col([
+            dash.page_container
+            ],
+            style=CONTENT_STYLE, width={'size':12})
+        ])
+    ])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
